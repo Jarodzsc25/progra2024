69476662c87cd995bc46622a262b8f87e6589e34
LabF103_PC23 <cadizsalanovajarodzarian@gmail.com>
2024-02-23 08:30:35 -0400

    1listo

--- src/ejemplos/suma.java
+++ src/ejemplos/suma.java
@@ -1,7 +1,18 @@
 package ejemplos;
 
+import java.util.Scanner;
+
 public class suma {
     public static void main(String[] args) {
-        class
+      int A;
+      int B;
+      int C;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("ingrese el primer dato entero:");
+        A = sc.nextInt();
+        System.out.println("ingrese el segundo dato entero:");
+        B = sc.nextInt();
+        C=A+B;
+        System.out.println("la suma es: "+C);
     }
 }
