6bed86bdc2face1605358cbec969f1f2f90e3dfb
LabF103_PC23 <cadizsalanovajarodzarian@gmailcom>
2024-03-01 08:25:18 -0400

    listo

---
+++ src/ejemplos3/ejem4_1.java
@@ -0,0 +1,18 @@
+package ejemplos3;
+
+import java.util.Scanner;
+
+public class ejem4_1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int suma = 0;
+        int contador=1;
+        while(contador<=10){
+            System.out.println("ingrese un valor numerico: ");
+            int valor = sc.nextInt();
+            suma=suma+valor;
+            contador=contador+1;
+        }
+        System.out.println("la suma es: "+suma);
+    }
+}
