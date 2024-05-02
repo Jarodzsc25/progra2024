a66577b67f969d8442fe6ebe28d1819cd21cd28e
jarod <cadizsalanovajarodzarian@gmail.com>
2024-05-02 17:24:11 -0400

    hoy

---
+++ src/mientras/pila_vacia.java
@@ -0,0 +1,16 @@
+package mientras.progra2024.src.mientras;
+import java.util.Stack;
+public class pila_vacia {
+    public static void main(String[] args) {
+        /*pila vacia*/
+        Stack<String> pila = new Stack<>();
+        System.out.println("vacía");
+        /* al añadir datos
+        pila.push("gato");
+        if (pila.isEmpty()) {
+            System.out.println("vacio");
+        } else {
+            System.out.println("tiene elementos");
+        }*/
+    }
+}
